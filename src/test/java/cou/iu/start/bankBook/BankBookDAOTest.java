36732e00721e1bc0b19fbf53d9a5bb7506a2eecd
kdy991101 <kdy991101@naver.com>
2022-08-18 15:23:47 +0900

    ..

--- src/test/java/cou/iu/start/bankBook/BankBookDAOTest.java
+++ src/test/java/cou/iu/start/bankBook/BankBookDAOTest.java
@@ -2,19 +2,46 @@ package cou.iu.start.bankBook;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.List;
+
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.iu.start.bankBook.BankBookDAO;
+import com.iu.start.bankBook.BankBookDTO;
 
 import cou.iu.start.MyAbstractTest;
 
+
 public class BankBookDAOTest extends MyAbstractTest{
+	
 
+	@Autowired//DAO에생성된 객체를 주입
+	private BankBookDAO bankBookDAO;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getListTest() throws Exception {
+	 List<BankBookDTO> ar = bankBookDAO.getList();
 	}
-
+	
 	@Test
-	public void setBankbookTest() {
+	public void setBankBookTest() throws Exception{
+		BankBookDTO bankBookDTO = new BankBookDTO();
+		Calendar ca = Calendar.getInstance();	
+		bankBookDTO.setBookName("--");
+		bankBookDTO.setBookNum(ca.getTimeInMillis());
+		bankBookDTO.setBookRate(1.1);
+		bankBookDTO.setBookSale(1);
+		bankBookDTO.B
 		
 	}
+	
+	@Test
+	public void getDetailTest() throws Exception{
+		BankBookDTO bankBookDTO = bankBookDAO.getDetail();
+	}
+	
+	
+	
 }
---
+++ src/test/java/cou/iu/start/bankmembers/BankMembersDAOTest.java
@@ -0,0 +1,39 @@
+package cou.iu.start.bankmembers;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.iu.start.bankMembers.BankMembersDAO;
+
+public class BankMembersDAOTest {
+
+	@Autowired
+	private BankMembersDAO bankMembersDAO;
+	
+	@Test
+	public void setJoinTest()throws Exception{
+		int result = bankMembersDAO.setJoin(null);
+		throw new Exception();
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void getloginTest()throws Exception{
+		Object obj = bankMembersDAO.getlogin(null);
+		assertNull(obj);
+	}
+	
+	@Test
+	public void getSearch()throws Exception{
+		
+		List<Object> ar = bankMembersDAO.getSearchByID(null);
+		
+		assertNotEquals(0, ar.size());
+	}
+}
+
+}
